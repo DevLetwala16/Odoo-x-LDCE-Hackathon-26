b75c0ad56d605ee4f056be9fe5ed79d809f6cbf3
Dev Letwala <letwala.dev@gmail.com>
2026-08-21 16:27:13 +0530

    first name commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Dev Letwala</h1>
+          <h1>Odoo x LDCE Hackathon 26</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
